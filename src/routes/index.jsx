21971b3de8adaefd0eee41b1bc86ac8de03372bb
Irupe <lilianirupemeza@gmail.com>
2023-03-22 18:58:15 -0300

    add route /login

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Landing from '../pages/Landing';
-import PanelAdmin from '../pages/PanelAdmin';
 import ContainerForm from '../components/ContainerForm';
 import ContainerBackoffice from '../components/Backoffice/ContainerBackoffice';
-import DashboardTransactions from '../components/Backoffice/DashboardTransactions';
-import DashoardCotiz from '../components/Backoffice/DashoardCotiz';
 import ContentDashboard from '../components/Backoffice/ContentDashboard';
+import Login from '../pages/Login';
 
 
 const AppRoutes = () => (
@@ -16,7 +14,7 @@ const AppRoutes = () => (
         <Route path='/form' element={<ContainerForm/>}/>       
         <Route path='/panelAdmin/transacciones' element={<ContentDashboard/>}/>  
         <Route path='/panelAdmin/cotizaciones' element={<ContentDashboard/>}/>  
-
+        <Route path='/login' element={<Login/>}/>  
     </Routes>
 );
 export default AppRoutes;
